c97925ae941c2a1ee96390b1f4c434ed00b2b058
Carlos Vinicius Ananias <carlos.oliveira@penseavanti.com.br>
2021-05-17 21:09:24 -0300

    Add createTodo

---
+++ node/middlewares/todo/createTodo.ts
@@ -0,0 +1,14 @@
+import parser from 'co-body'
+
+export async function createTodo(ctx: Context, next: () => Promise<any>) {
+  const data = await parser(ctx.req)
+
+  console.info('data', data)
+  data.id = 12
+
+  ctx.status = 200
+  ctx.body = data
+  ctx.set('Cache-Control', 'no-cache')
+
+  await next()
+}
